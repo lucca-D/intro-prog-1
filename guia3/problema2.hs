3c6d63e842237eeb1ecb57ae15b802976c08448b
lucca <ldassler99@gmail.com>
2025-09-16 22:37:04 -0300

    avance pa

--- guia3/problema2.hs
+++ guia3/problema2.hs
@@ -1,3 +1,4 @@
+import Control.Concurrent (yield)
 --Especificar e implementar las siguientes funciones, incluyendo su signatura.
 
 --a) absoluto: calcula el valor absoluto de un numero entero.
@@ -21,8 +22,10 @@ maximoAbsoluto x y  | (absoluto x) > (absoluto y) = absoluto x
 
 -- c) maximo3: devuelve el maximo entre tres numeros enteros
 -- problema     maximo3(x:Z,y:Z,w:Z):Z {
---                  requiere(TRUE)
---                  asegura(requiere sera el valor maximo entre los 3 numeros enteros)
+--                  requiere{TRUE}
+--                  asegura{requiere sera el valor maximo entre los 3 numeros enteros}
+--       mejor asi: asegura{(res = x) V (res = y) V (res = w)}
+--                  asegura{(res ≥ x) ∧ (res ≥ y) ∧ (res ≥ z)}
 --              }
 
 maximo3 :: Integer -> Integer -> Integer -> Integer
@@ -31,4 +34,57 @@ maximo3 x y w   | (x >= y) && (x >= w) = x
                 | otherwise = w
 
 -- d) algunoEsCero: dados dos numeros racionales, decide si alguno es igual a 0 (resolverlo con y sin pattern matching)
---
+-- problema algunoEsCero(x : Q, y: Q) : Boolean {
+--              requiere{TRUE}
+--              asegura{res sera True si x=0 o y=0}
+--              asegura{res sera False si (x /= 0) y (y/=0)}
+--}
+
+algunoEsCero :: Float -> Float -> Bool
+algunoEsCero x y    | (x == 0) || (y == 0) = True
+                    | otherwise = False
+-- con pattern matching:
+algunoEsCero2 :: Float -> Float -> Bool
+algunoEsCero2 0 _ = True
+algunoEsCero2 _ 0 = True
+algunoEsCero2 _ _ = False -- va evaluando paso a paso; si el 1er argumento no es 0 entonces va al 2do, si tampoco es entonces llega al 3ro, el cual le dice que resultaria False
+
+-- e) ambosSonCero: dados dos numeros racionales, decide si ambos son iguales a 0 (resolverlo con y sin pattern matching).
+--  problema ambosSonCero(x:Q , y:Q) : Bool {
+--      requiere{TRUE}
+--      asegura{req sera True si x = y = 0}
+--      asegura{req sera False si x /= 0 o y /= 0}
+--}
+
+ambosSonCero :: Float -> Float -> Bool
+ambosSonCero x y    | (x == y) && (x == 0 )= True
+                    | otherwise = False
+ambosSonCero2 :: Float -> Float -> Bool
+ambosSonCero2 0 0 = True
+ambosSonCero2 _ _ = False
+
+-- g) sumaDistintos: que dados tres numeros enteros calcule la suma sin sumar repetidos (si los hubiera)
+--  problema sumaDistintos(x:Z,y:Z,w:Z) : Z {
+--      requiere{True}
+--      asegura{res sera 0 si los 3 parametros son iguales}
+--      asegura{si 2 parámetros son iguales, res será igual al distinto}
+--      asegura{si todos los parámetros son distintos, res sera x+y+w}
+--  }
+
+sumaDistintos :: Integer -> Integer -> Integer -> Integer
+sumaDistintos x y z | x == y && y == z = 0
+                    | x /= y && y /= z && x /= z = x+y+z
+                    | x==y = z
+                    | x==z = y
+                    | z==y = x
+
+-- h) esMultiploDe: dados dos numeros naturales, decide si el primero es multiplo del segundo
+-- problema esMultiploDe (x:Z,y:Z): Bool {
+--      requiere{los dos parámetros deben ser mayor a 0}
+--      asegura{res es True si un parámetro es múltiplo del otro}
+--      asegura{res es False si los parámetros no son múltiplos}
+--}
+
+esMultiploDe :: Integer -> Integer -> Bool
+esMultiploDe x y    | (x>0) && (y>0) && (mod x y == 0) = True
+                    | otherwise = False
--- guia3/problemaActual.hs
+++ guia3/problemaActual.hs
@@ -2,6 +2,7 @@
 -- correr pw con admin y luego
 -- ghci
 -- :load "C:\\Scripts\\scripts_facultad\\intro-prog-1\\guia3\\problemaActual.hs"
+-- :load "D:\\facultad\\gitRepo\\intro-prog-1\\guia3\\problemaActual.hs"
 -- en caso de cambios :reload
 absoluto:: Integer -> Integer
 absoluto x  | x >= 0 = x
@@ -13,9 +14,40 @@ maximo3 :: Integer -> Integer -> Integer -> Integer
 maximo3 x y w   | (x >= y) && (x >= w) = x
                 | (y >= x) && (y >= w) = y
                 | otherwise = w
+algunoEsCero :: Float -> Float -> Bool
+algunoEsCero x y    | (x == 0) || (y == 0) = True
+                    | otherwise = False
+algunoEsCero2 :: Float -> Float -> Bool
+algunoEsCero2 0 _ = True
+algunoEsCero2 _ 0 = True
+algunoEsCero2 _ _ = False
+
+ambosSonCero :: Float -> Float -> Bool
+ambosSonCero x y    | (x == y) && (x == 0 )= True
+                    | otherwise = False
+ambosSonCero2 :: Float -> Float -> Bool
+ambosSonCero2 0 0 = True
+ambosSonCero2 _ _ = False
+
+sumaDistintos :: Integer -> Integer -> Integer -> Integer
+sumaDistintos x y z | x == y && y == z = 0
+                    | x /= y && y /= z && x /= z = x+y+z
+                    | x==y = z
+                    | x==z = y
+                    | z==y = x
+
+esMultiploDe :: Integer -> Integer -> Bool
+esMultiploDe x y    | (x>0) && (y>0) && (mod x y == 0) = True
+                    | otherwise = False
+
 main:: IO()
 main = do
-    print(absoluto 5)
-    print(absoluto (-5))
-    print(maximoAbsoluto 5 (-6))
-    print(maximo3 3 6 9)
+    --print(absoluto (-5))
+    --print(maximoAbsoluto 5 (-6))
+    --print(maximo3 3 6 9)
+    --print(algunoEsCero 5.5 0)
+    --print(algunoEsCero2 5.5 1.1)
+    print(ambosSonCero 0 1)
+    print(ambosSonCero2 1 0)
+    print(sumaDistintos 2 2 2)
+    print(esMultiploDe 10 2)
