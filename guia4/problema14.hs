13dd02e854a0b3753d1caf036633cc94a70395a9
lucca <ldassler99@gmail.com>
2025-09-24 00:01:37 -0300

    pendiente p14

---
+++ guia4/problema14.hs
@@ -0,0 +1,5 @@
+{--
+ Ejercicio 14. Especificar e implementar una funcion sumaPotencias :: Integer->Integer->Integer->Integer que
+ dados tres naturales q,n,m sume todas las potencias de la forma q^(a+b) con 1 ≤ a ≤ n y 1 ≤ b ≤ m
+--}
+
