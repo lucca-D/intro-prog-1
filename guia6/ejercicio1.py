c02d8c77f8021a271937cfd5ac26568f498c3283
zxBAI DAmore,Lucca (IT INF) EXTERNAL <lucca.d_amore.ext@boehringer-ingelheim.com>
2025-10-10 17:32:04 -0300

    arranco guia PYTHON

---
+++ guia6/ejercicio1.py
@@ -0,0 +1,32 @@
+import math
+# Ejercicio 1. Definir las siguientes funciones y procedimientos:
+# 1. raizDe2(): que devuelva la ra ız cuadrada de 2 con 4 decimales. Ver funci ́on round
+
+def raizDe2(x: int) -> int :
+    return round(math.sqrt(x),2)
+
+# 2. problema imprimir hola () {
+#requiere: { True }
+#asegura: { imprime ’hola’por consola}
+#}
+
+def imprimirHola():
+    print("Hola")
+imprimirHola()
+
+# 3. imprimir un verso(): que imprima un verso de una canci ́on que vos elijas, respetando los saltos de l ́ınea.
+def imprimir_un_verso():
+    print("tengo que aprender a volar")
+    print("entre tanta gente de pie")
+imprimir_un_verso()
+
+# 4. factorial de dos()
+#problema factorial 2 () : Z {
+#requiere: { True }
+#asegura: {res = 2!}
+#}
+
+def factorial_de_dos():
+    print(2)
+
+# el resto son FACILITOS
